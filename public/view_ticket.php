0ccdb89bdd65b9450e70d0cf78978dbe3f0233e0
Andrii Fedorov <andrii.fedorov@infis.cz>
2026-06-10 19:37:10 +0200

    feat: implement engineer assignment and database-driven ticket logs

--- public/view_ticket.php
+++ public/view_ticket.php
@@ -17,6 +17,26 @@ if(!isset($_GET['id']) || empty($_GET['id'])){
 
 $ticketId = (int)$_GET['id'];
 
+//upd
+if($_SERVER['REQUEST_METHOD'] === 'POST' && $role === 'Admin' && isset($_POST['assign_engineer'])){
+    $engineerId = (int)$_POST['engineer_id'];
+   if($engineerId > 0){
+    $stmtUpdate = $pdo->prepare("UPDATE tickets SET engineer_id = ? WHERE id = ?");
+    $stmtUpdate->execute([$engineerId, $ticketId]);
+
+    $stmtEngName = $pdo->prepare("SELECT name FROM users WHERE id = ?");
+    $stmtEngName->execute([$engineerId]);
+    $engineerName = $stmtEngName->fetchColumn();
+
+
+    $stmtLog = $pdo->prepare("INSERT INTO ticket_logs (ticket_id, action_text) VALUES (?, ?)");
+    $stmtLog->execute([$ticketId, "Admin assigned engineer: " . $engineerName]);
+
+    header("Location: view_ticket.php?id=" . $ticketId);
+    exit();
+   }
+}
+//upd
 $stmt = $pdo->prepare("
     SELECT t.*,
         u.name AS client_name,
@@ -43,6 +63,16 @@ if($role === 'Client' && (int)$ticket['client_id'] !== $userId){
     echo "403 Forbidden. You do not have permission to view this application.";
     exit();
 }
+
+$engineers = [];
+if($role === 'Admin'){
+    $stmtEng = $pdo->query("SELECT id, name FROM users WHERE role = 'Engineer' ORDER BY name ASC");
+    $engineers = $stmtEng->fetchall();
+}
+
+$stmtLogs = $pdo->prepare("SELECT action_text, created_at FROM ticket_logs WHERE ticket_id = ? ORDER BY created_at DESC");
+$stmtLogs->execute([$ticketId]);
+$logs = $stmtLogs->fetchAll();
 
 require_once __DIR__ . '/../views/header.php';
 ?>
@@ -87,9 +117,40 @@ require_once __DIR__ . '/../views/header.php';
             </span>
        </div>
 
+       <?php if ($role === 'Admin'): ?>
+            <div class="border-t border-gray-800 pt-4 mt-4">
+                <form action="view_ticket.php?id=<?= $ticketId ?>" method="POST" class="space-y-2">
+                    <label for="engineer_id" class="block text-xs text-gray-400">Assign or Change Engineet:</label>
+                    <div class="flex gap-2">
+                        <select name="engineer_id" id="engineer_id" required class="bg-gray-950 border border-gray-800 rounded px-3 py-1.5 text-white focus:outline-none text-xs">
+                            <option value="">Select Engineer</option>
+                            <?php foreach ($engineers as $eng): ?>
+                                <option value="<?= $eng['id'] ?>" <?= ((int)$ticket['engineer_id'] === (int)$eng['id']) ? 'selected' : '' ?>>
+                                    <?= htmlspecialchars($eng['name'], ENT_QUOTES, 'UTF-8') ?>
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                        <button type="sumbit" name="assign_engineer" class="bg-orange-600 hover:bg-orabge-500 text-white text-xs px-4 py-1.5 rounded transition">
+                            Assign
+                        </button>
+                    </div>
+                </form>
+            </div>
+        <?php endif; ?>
+
        <div class="border-t border-gray-800 pt-4">
             <span class="text-xs text-gray-400 block mb-1">Detailed Description:</span>
             <p class="text-gray-300 whitespace-pre-wrap leading-relaxed"><?= htmlspecialchars($ticket['description'], ENT_QUOTES, 'UTF-8') ?></p>
+       </div>
+        
+       <div class="border-t border-gray-800 pt-4">
+            <span class="text-xs text-gray-400 block-mb-2">Application History Logs:</span>
+            <div class="space-y-1 text-xs font-mono text-gray-500">
+                <div>[<?= date('d.m.Y H:i', strtotime($ticket['created_at'])) ?>] Ticket created by client.</div>
+                <?php foreach($logs as $log): ?>
+                    <div>[<?= date('d.m.Y H:i', strtotime($log['created_at'])) ?>] <?= htmlspecialchars($log['action_text'], ENT_QUOTES, 'UTF-8') ?></div>
+                <?php endforeach; ?>
+            </div>
        </div>
     </div>
 </div>
